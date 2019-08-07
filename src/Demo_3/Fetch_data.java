9032840f5bae148f25e517d1d58fe1a5a8ce7ebd
dhruv_khandelwal <dhruv.khandelwal@spglobal.com>
2019-08-07 12:37:45 +0530

    Added new test class for property file

---
+++ src/Demo_3/Fetch_data.java
@@ -0,0 +1,26 @@
+package Demo_3;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Fetch_data {
+
+	public static void main(String[] args) throws IOException {
+
+		//Pull data from a property file and use it for testing
+		
+		Properties prob = new Properties();
+		FileInputStream fs = new FileInputStream("C:\\TEST_newPace\\TEST_Selenium\\src\\TEST_Data.properties");
+	    
+		prob.load(fs);
+		System.out.println(prob.get("Name"));
+		System.out.println(prob.get("Class"));
+		System.out.println(prob.get("Rollno"));
+		System.out.println(prob.get("browser"));
+		
+		
+	}
+
+}
